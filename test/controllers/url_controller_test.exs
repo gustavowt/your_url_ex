137777d8b9a273474fdd6364f799880c2d6a98b5
Gustavo Warmling Teixeira <gustavowt@gmail.com>
2017-12-01 19:53:07 -0200

    Fix Url controller action show test

--- test/controllers/url_controller_test.exs
+++ test/controllers/url_controller_test.exs
@@ -2,8 +2,8 @@ defmodule UrlShortner.UrlControllerTest do
   use UrlShortner.ConnCase
 
   alias UrlShortner.Url
-  @valid_attrs %{original_url: "some content", short_url: "some content"}
-  @invalid_attrs %{}
+  @valid_attrs %{ clicks: 20, original_url: "http://google.com" }
+  @invalid_attrs %{ original_url: "google.com" }
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
@@ -15,11 +15,13 @@ defmodule UrlShortner.UrlControllerTest do
   end
 
   test "shows chosen resource", %{conn: conn} do
-    url = Repo.insert! %Url{}
-    conn = get conn, url_path(conn, :show, url)
-    assert json_response(conn, 200)["data"] == %{"id" => url.id,
+    url = Repo.insert! %Url{ clicks: 20, original_url: "http://yahoo.com", url_hash: "AA223344" }
+
+    conn = get conn, url_path(conn, :show, %Url{id: url.url_hash})
+    assert json_response(conn, 200)["data"] == %{
+      "clicks" => url.clicks,
       "original_url" => url.original_url,
-      "short_url" => url.short_url}
+      "short_url" => "#{UrlShortner.Endpoint.url}/#{url.url_hash}"}
   end
 
   test "renders page not found when id is nonexistent", %{conn: conn} do
