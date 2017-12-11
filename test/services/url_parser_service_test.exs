f5e42640f9bdceb5c87e5c1108fb65dbb26d5b22
Gustavo Warmling Teixeira <gustavowt@gmail.com>
2017-12-11 10:09:42 -0200

    Add new url parser test
    testing with diferent protocol

--- test/services/url_parser_service_test.exs
+++ test/services/url_parser_service_test.exs
@@ -10,4 +10,8 @@ defmodule UrlShortner.UrlParserServiceTest do
   test "#parse_url, should parse url with no scheme" do
     assert UrlParserService.parse_url("yahoo.com") == "http://yahoo.com"
   end
+
+  test "#parse_url, should parse url with diferent protocol" do
+    assert UrlParserService.parse_url("ftp://yahoo.com") == "ftp://yahoo.com"
+  end
 end
