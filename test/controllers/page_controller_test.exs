ce21cfc995d9f1f3d862f9f601d551ed739eddda
Gustavo Warmling Teixeira <gustavowt@gmail.com>
2017-12-21 14:26:33 -0200

    Update PageControllerTest

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,12 +3,29 @@ defmodule UrlShortner.PageControllerTest do
 
   alias UrlShortner.Url
 
-  test "GET /", %{conn: conn} do
+  test "GET /:id redirect to given url", %{conn: conn} do
     url = Repo.insert! %Url{ clicks: 20, original_url: "http://yahoo.com", url_hash: "AA223344" }
 
-    conn = get conn, "/", id: url.url_hash
+    conn = get conn, "/#{url.url_hash}"
+
     assert redirected_to(conn) ==  "http://yahoo.com"
 
-    assert Repo.get(url.id).clicks == 21
+    assert Repo.get(Url, url.id).clicks == 21
+  end
+
+  test 'GET /:id redirect even incomplete url', %{conn: conn} do
+    url = Repo.insert! %Url{ clicks: 20, original_url: "yahoo.com", url_hash: "AA223344" }
+
+    conn = get conn, "/#{url.url_hash}"
+
+    assert redirected_to(conn) ==  "http://yahoo.com"
+
+    assert Repo.get(Url, url.id).clicks == 21
+  end
+
+  test 'GET /:id renders page not found when id is not found', %{conn: conn} do
+    assert_error_sent 404, fn ->
+      get conn, "/AAVVCC"
+    end
   end
 end
