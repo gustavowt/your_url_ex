722e428d230d81f548a794a9939657ffffdee875
Gustavo Warmling Teixeira <gustavowt@gmail.com>
2018-01-10 18:43:40 -0200

    Fix shortner service indentation

--- test/services/shortner_service_test.exs
+++ test/services/shortner_service_test.exs
@@ -4,7 +4,7 @@ defmodule UrlShortner.ShortnerServiceTest do
   test "#create_short_url, create a new url entry" do
     params = %{ "original_url" => "http://www.pudim.com.br" }
 
-    {:ok,  url } = UrlShortner.ShortnerService.create_short_url(params)
+    { :ok, url } = UrlShortner.ShortnerService.create_short_url(params)
 
     assert url.clicks == 0
     assert url.url_hash != nil
